f2a99e72980668159cec2f0a7a83fc7fed01ef8a
EfratAnsbacher <38215274465@mby.co.il>
2026-04-17 05:54:54 +0200

    6-82

--- server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20719bd59ba04a067fda7c83ef57efe14d538252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74444d764cb1e3d92419736589934300277d39c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
